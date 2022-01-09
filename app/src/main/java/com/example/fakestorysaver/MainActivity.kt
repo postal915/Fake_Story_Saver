6b4f38cc5f857e0b974bda75f9e679c57e38108f
Stas Latyshev <postal915@gmail.com>
2022-01-09 21:50:58 +0200

    refactoring

--- app/src/main/java/com/example/fakestorysaver/MainActivity.kt
+++ app/src/main/java/com/example/fakestorysaver/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.fakestorysaver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fakestorysaver.databinding.ActivityMainBinding
 import com.example.fakestorysaver.del.NumberAdapter
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: NumberAdapter
-    private lateinit var tabLayout: TabLayout
     private val tabNames: Array<String> = arrayOf("story", "following", "feed")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +20,7 @@ class MainActivity : AppCompatActivity() {
         adapter = NumberAdapter(this)
         binding.pager.adapter = adapter
 
-        tabLayout = findViewById(R.id.tabLayouts)
-        TabLayoutMediator(tabLayout, binding.pager) { tab, position ->
+        TabLayoutMediator(binding.tabLayouts, binding.pager) { tab, position ->
             tab.text = tabNames[position]
         }.attach()
 
