184045b1bc2b01efe19bbb04d727a67261b1317a
Stas Latyshev <postal915@gmail.com>
2022-01-09 18:06:48 +0200

    implemented tab switching

--- app/src/main/java/com/example/fakestorysaver/MainActivity.kt
+++ app/src/main/java/com/example/fakestorysaver/MainActivity.kt
@@ -2,15 +2,31 @@ package com.example.fakestorysaver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 import com.example.fakestorysaver.databinding.ActivityMainBinding
+import com.example.fakestorysaver.del.NumberAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NumberAdapter
+    private lateinit var tabLayout: TabLayout
+    private val tabNames: Array<String> = arrayOf("story", "following", "feed")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = NumberAdapter(this)
+        binding.pager.adapter = adapter
+
+        tabLayout = findViewById(R.id.tabLayouts)
+        TabLayoutMediator(tabLayout, binding.pager) { tab, position ->
+            tab.text = tabNames[position]
+        }.attach()
+
     }
 }
---
+++ app/src/main/java/com/example/fakestorysaver/del/NumberFragment.kt
@@ -0,0 +1,29 @@
+package com.example.fakestorysaver.del
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.fakestorysaver.R
+
+const val ARG_OBJECT = "object"
+
+class NumberFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_number, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+            val textView: TextView = view.findViewById(R.id.textView)
+            textView.text = getInt(ARG_OBJECT).toString()
+        }
+    }
+
+}
